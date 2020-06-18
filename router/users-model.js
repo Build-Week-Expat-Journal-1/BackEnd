346594dce500f42e336a5f2f9c31f4983db8aeaa
squashgray <squashgray@gmail.com>
2020-06-17 19:12:06 -0700

    database tables/ migrations

---
+++ router/users-model.js
@@ -0,0 +1,29 @@
+const db = require("../data/dbConfig");
+
+module.exports = {
+  findUsers,
+  add,
+  findBy,
+  findById,
+};
+
+function findUsers() {
+  return db("users").select("id", "username", "password");
+}
+
+function findBy(filter) {
+  return db("users").select("id", "username", "password").where(filter);
+}
+
+function add(user) {
+  return db("users")
+    .insert(user, "id")
+    .then((ids) => {
+      const [id] = ids;
+      return findById(id);
+    });
+}
+
+function findById(id) {
+  return db("users").select("id", "username").where({ id }).first();
+}
