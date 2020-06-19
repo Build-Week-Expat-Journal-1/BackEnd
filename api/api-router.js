db01f8204f2a265e7ec5b886092cac520ae211e5
Daniel Hermansen <daniel.j.hermansen@gmail.com>
2020-06-18 22:27:31 -0600

    Added a get endpoint for the users router to test the login and restricted middleware. Made minor changes to messages in the auth-router.

--- api/api-router.js
+++ api/api-router.js
@@ -4,6 +4,7 @@ const authRouter = require("../auth/auth-router");
 const usersRouter = require("../router/users-router");
 
 router.use("/auth", authRouter);
+router.use("/users", usersRouter);
 
 router.get("/", (req, res) => {
   res.json({ api: "Server Online" });
