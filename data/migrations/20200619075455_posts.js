c7048e5b0c659e046fe3df30848856b39c88e60d
squashgray <squashgray@gmail.com>
2020-06-19 08:37:38 -0700

    posts table

---
+++ data/migrations/20200619075455_posts.js
@@ -0,0 +1,20 @@
+const moment = require("moment");
+
+exports.up = async function (knex) {
+  await knex.schema.createTable("posts", (tbl) => {
+    tbl.increments("id").notNullable();
+    tbl.string("topic").notNullable();
+    tbl.text("description").notNullable();
+    tbl
+      .string("posted_date")
+      .defaultTo(moment().format("YYYY-MM-DD HH:mm:ss"))
+      .notNullable()
+      .unique();
+    tbl.integer("reported_by").notNullable().references("id").inTable("users");
+    tbl.string("photo").notNullable();
+  });
+};
+
+exports.down = async function (knex) {
+  await knex.schema.dropTableIfExists("posts");
+};
