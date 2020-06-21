6ff38df0409cdfb4c7ea5bc5ae3c35eea8ae396d
squashgray <squashgray@gmail.com>
2020-06-21 11:34:20 -0700

    posts router added

--- api/api-router.js
+++ api/api-router.js
@@ -2,9 +2,11 @@ const router = require("express").Router();
 
 const authRouter = require("../auth/auth-router");
 const usersRouter = require("../users/users-router");
+const postsRouter = require("../posts/posts.router");
 
 router.use("/auth", authRouter);
 router.use("/users", usersRouter);
+router.use("/posts", postsRouter);
 
 router.get("/", (req, res) => {
   res.json({ api: "Server Online" });
